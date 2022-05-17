b3a48db0e630a02d52f156b12d17ba117d3bc568
Truman Knight <cvilletruman@gmail.com>
2022-05-17 04:14:09 -0400

    Get into the red part 2

--- test/index_test.js
+++ test/index_test.js
@@ -3,9 +3,21 @@ var Calculate =  require('../index.js')
 
 describe('Calculate', () => {
   describe('.factorial', () => {
+      afterEach(() => {
+          delete inputNumber;
+          delete expectedResult;
+          delete result;
+      })
     it('tests if the output of 5! is equal to 120', () => {
         const inputNumber = 5;
         const expectedResult = 120;
+
+        const result = Calculate.factorial(inputNumber);
+        assert.equal(result, expectedResult);
+    })
+    it('tests if the output of 6! is equal to 720', () => {
+        const inputNumber = 6;
+        const expectedResult = 720;
 
         const result = Calculate.factorial(inputNumber);
         assert.equal(result, expectedResult);
