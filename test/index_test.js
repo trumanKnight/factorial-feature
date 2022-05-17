9cced68778a114cb2c6836b22901b4c7ad4b37b1
Truman Knight <cvilletruman@gmail.com>
2022-05-17 04:05:09 -0400

    Get into the red part 1a

--- test/index_test.js
+++ test/index_test.js
@@ -3,6 +3,8 @@ var Calculate =  require('../index.js')
 
 describe('Calculate', () => {
   describe('.factorial', () => {
-
+    it('tests if the output of 5! is equal to 120', () => {
+        
+    })
   });
 });
