fc6f13b1b21be87d0f3a93673f313f4034e8194e
Truman Knight <cvilletruman@gmail.com>
2022-05-17 04:06:29 -0400

    Get into the red part 1b

--- test/index_test.js
+++ test/index_test.js
@@ -4,7 +4,7 @@ var Calculate =  require('../index.js')
 describe('Calculate', () => {
   describe('.factorial', () => {
     it('tests if the output of 5! is equal to 120', () => {
-        
+        assert.equal(Calculate.factorial(5), 120);
     })
   });
 });
